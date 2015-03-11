f22ab897a2a8bf54afbc44032c6f1c52d5c15109
Swastik <swastik@aceteq.co>
2015-03-11 20:04:55 +0530

    Cleaned up

--- components/App.jsx
+++ components/App.jsx
@@ -7,37 +7,10 @@ var DocumentTitle = require('react-document-title');
 var RouteHandler  = Router.RouteHandler;
 var Link          = Router.Link;
 
-var data  = require('../public/data/places');
-var title = "Some places in Italy";
-
 var App = React.createClass({
 
-  getDefaultProps: function () {
-    return { places: data };
-  },
-
   render: function () {
-    var links = this.props.places.map(function (place) {
-      return (
-        <li key={"place-" + place.id}>
-          <Link to="place" params={{ id: place.id }}>{place.name}</Link>
-        </li>
-      );
-    });
-    return (
-      <DocumentTitle title={ title }>
-        <div className="app">
-          <h1>{ title }</h1>
-          <ul className="master">
-            { links }
-            <Link to="index"><small>(back to index)</small></Link>
-          </ul>
-          <div className="detail">
-            <RouteHandler />
-          </div>
-        </div>
-      </DocumentTitle>
-    );
+    return null
   }
 });
 
